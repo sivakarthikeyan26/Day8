fde43ab3d5d2104fe90847594e3042a1f9c88191
Your Name <you@example.com>
2022-06-29 04:47:17 +0530

    two lines full comparison is implemented

--- src/line_comparison/Computation.java
+++ src/line_comparison/Computation.java
@@ -1,6 +1,5 @@
 package line_comparison;
 
-import java.util.Scanner;
 
 public class Computation {
 	int x1,y1,x2,y2,x3,y3,x4,y4;
@@ -10,17 +9,15 @@ public class Computation {
 	}
 	public void input() {
 		System.out.println("Enter x1,y1,x2 and y2 for line 1");
-		Scanner sc = new Scanner(System.in);
-		x1=sc.nextInt();
-		y1=sc.nextInt();
-		x2=sc.nextInt();
-		y2=sc.nextInt();
+		x1=Line_comparison.sc1.nextInt();
+		y1=Line_comparison.sc1.nextInt();
+		x2=Line_comparison.sc1.nextInt();
+		y2=Line_comparison.sc1.nextInt();
 		System.out.println("Enter x3,y3,x4 and y4 for line 2");
-		x3=sc.nextInt();
-		y3=sc.nextInt();
-		x4=sc.nextInt();
-		y4=sc.nextInt();
-		sc.close();
+		x3=Line_comparison.sc1.nextInt();
+		y3=Line_comparison.sc1.nextInt();
+		x4=Line_comparison.sc1.nextInt();
+		y4=Line_comparison.sc1.nextInt();
 	}
 	public void distance() {
 		line1_distance = (float) Math.sqrt(((x2 - x1) * (x2 - x1) ) + ((y2 - y1) *(y2 - y1)));
@@ -28,11 +25,14 @@ public class Computation {
 		System.out.println("the length of line 1 is "+line1_distance+" and line 2 is "+line2_distance);
 	}
 	public void compare() {
-		if(line1_distance == line2_distance) {
-			System.out.println("the two lines are equal");
+		if(line1_distance > line2_distance) {
+			System.out.println("line1 is greater");
+		}
+		else if(line1_distance < line2_distance) {
+			System.out.println("line2 is greater");
 		}
 		else {
-			System.out.println("they are not equal");
+			System.out.println("the two lines are equal");
 		}
 	}
 
--- src/line_comparison/Line_comparison.java
+++ src/line_comparison/Line_comparison.java
@@ -1,13 +1,35 @@
 package line_comparison;
 
-public class Line_comparison {
+import java.util.Scanner;
 
+public class Line_comparison {
+	
+	public static final Scanner sc1 = new Scanner(System.in);
 	public static void main(String[] args) {
+		boolean flag=false;
+		int choice;
 		Computation comp = new Computation();
 		comp.welcome();
-		comp.input();
-		comp.distance();
-		comp.compare();
+		while(flag!=true) {
+			System.out.println("enter the choice : \n1.Enter the inputs\n2.compute only distance of two lines\n3.compare two lines\n4.exit the program");
+		    choice=sc1.nextInt();
+			switch(choice) {
+			case 1:
+				comp.input();
+				break;
+			case 2:
+				comp.distance();
+				break;
+			case 3:
+				comp.compare();
+				break;
+			case 4:
+				sc1.close();
+				System.exit(0);
+			default :
+				System.out.println("enter the correct choice");
+			}
+		}
 
 	}
 
